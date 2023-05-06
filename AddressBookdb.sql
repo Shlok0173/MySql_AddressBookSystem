2ad5148d929fd5fd518ee0a9a70b5bab9b172dc5
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-05-06 09:46:54 +0530

    Added : UpdateData

--- AddressBookdb.sql
+++ AddressBookdb.sql
@@ -1,7 +1,8 @@
- ---------------------------- Uc3 ------------------------------
- desc addressbook; 
-insert into addressbook(firstNames,lastNames,address,city,state,zip,phoneNumber,email)values('Sidhi','Mishra','siwan','Siwan','Bihar',726089,9990898877,'sidhi1234@gmail.com'),
-('Amit','Mishra','EastLuc','Lucknow','UttarPardesh',867865,8929204444,'amit3@gmail.com'),
-('Sonu','Yadav','Up','EastUp','UttarPardesh',786934,9898776655,'sonu123@gmail.com');
+ ---------------------------- Uc4 ------------------------------
+ ------------ desc addressbook; 
+----------------------- insert into addressbook(firstNames,lastNames,address,city,state,zip,phoneNumber,email)values('Sidhi','Mishra','siwan','Siwan','Bihar',726089,9990898877,'sidhi1234@gmail.com'),
+------------------- ('Amit','Mishra','EastLuc','Lucknow','UttarPardesh',867865,8929204444,'amit3@gmail.com'), --------------
+----------------------------- ('Sonu','Yadav','Up','EastUp','UttarPardesh',786934,9898776655,'sonu123@gmail.com'); ----------------
 
+update addressbook set  lastNames='Tiwari',address='Patna',state ='Bihar',zip=112233,phoneNumber=9299000000 ,email='tiwari@gmail.com' where firstNames='Ajit';
 select * from addressbook;
