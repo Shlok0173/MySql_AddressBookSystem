ff248f43dfbca79fd3c0dd5d000f518f009a72ac
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-05-06 08:55:38 +0530

    Added: Insert Data

--- AddressBookdb.sql
+++ AddressBookdb.sql
@@ -1,1 +1,7 @@
+ ---------------------------- Uc3 ------------------------------
+ desc addressbook; 
+insert into addressbook(firstNames,lastNames,address,city,state,zip,phoneNumber,email)values('Sidhi','Mishra','siwan','Siwan','Bihar',726089,9990898877,'sidhi1234@gmail.com'),
+('Amit','Mishra','EastLuc','Lucknow','UttarPardesh',867865,8929204444,'amit3@gmail.com'),
+('Sonu','Yadav','Up','EastUp','UttarPardesh',786934,9898776655,'sonu123@gmail.com');
+
 select * from addressbook;
