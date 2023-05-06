fd702b31ea621ea84ccb22fb3daf2eabb2ba94d6
Shlok Kumar Mishra <shlok7869@gmail.com>
2023-05-06 21:25:18 +0530

    Added : Count Person By type

--- AddressBookdb.sql
+++ AddressBookdb.sql
@@ -1,2 +1,9 @@
---------------------------------- Uc8 ------------------------------
-select  firstNames  AS " FIRSTNAME" from addressbook  order by city desc ;
+--------------------------------- Uc9 ------------------------------
+------ alter table addressbook add type varchar(100);
+-------------- select*from addressbook;
+----------------- update addressbook set type='Friends' where firstNames='Shlok';
+----------------- update addressbook set type = 'Profession' where firstNames='Amit'; 
+
+------------------------------ Uc10 --------------------------
+select  count(type) As " Contats Person " from addressbook ;
+--------- select*from addressbook;
